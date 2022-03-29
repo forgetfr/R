9bfeb5e99a6057636db45303b201b9c3e835788c
Francis Forget <forgetfr@widowmaker.inuvola.ca>
2022-03-29 14:26:48 -0400

    First try

--- installation.sh
+++ installation.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 WORKINGDIR=`mktemp -p`
+VERSION="4.1.3"
+PATH_TO_INSTALL="/usr/local/R${VERSION}"
+PATH_TO_LINK="/usr/local/R"
+
 cd $WORKINGDIR
 
-wget https://cran.r-project.org/src/base/R-4/R-4.1.3.tar.gz
+wget https://cran.r-project.org/src/base/R-4/R-${VERSION}.tar.gz
+mkdir tar -zxf R-${VERSION}.tar.gz && cd R-${VERSION}
 
-tar -zxf R-4.1.3.tar.gz
+mkdir ${PATH_TO_INSTALL}
+./configure --prefix=${PATH_TO_INSTALL} --shlibs=enable
+make 
+make install
 
-cd 
-
+ln -s ${PATH_TO_INSTALL} ${PATH_TO_LINK}
 
 rm -rf $WORKINGDIR
